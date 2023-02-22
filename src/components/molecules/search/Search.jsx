e51f9f67416995f41816385321be716dec076653
Ana <117080170+anavgg@users.noreply.github.com>
2023-02-22 23:55:53 +0100

    searchbar

---
+++ src/components/molecules/search/Search.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from 'react';
+import "../search/search.css"
+// import {AiOutlineSearch} from 'react-icons/ai'
+
+const Search = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+  
+    const handleSearch = (event) => {
+      event.preventDefault();
+      console.log(searchTerm);
+      // Aquí se puede realizar acción de búsqueda con el término ingresado(??)
+    };
+  
+    const handleChange = (event) => {
+      setSearchTerm(event.target.value);
+    };
+  
+    const handleKeyDown = (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleSearch(event);
+      }
+    };
+  
+    return (
+      <div className="search-container">
+        <form>
+          <input
+            type="text"
+            placeholder="Buscar..."
+            value={searchTerm}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+          {/* <button type="button" onClick={handleSearch}>
+          <AiOutlineSearch/>
+          </button> */}
+        </form>
+      </div>
+    );
+  }
+  
+
+export default Search;
+
+
+
+
