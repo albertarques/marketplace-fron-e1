dcdd304bd57c260aa7a98456056a2601db2d9e84
Ana <117080170+anavgg@users.noreply.github.com>
2023-02-22 17:34:38 +0100

    aaaaa

--- src/components/molecules/navbar/Navbar.jsx
+++ src/components/molecules/navbar/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../../assets/img/logo.png";
+import style from "./navbar.module.css"
 
 
 const Navbar = () => (
-  <div className="navbarContainer">
-    <div className="navbar">
-      <div className="navbarBrand">
-      </div>
+  <div className={style.navbarContainer}>
+
+      <div className={style.navbarLogo}>
+      {/* <Link to="/"> */}
+          <img src={Logo} alt="Logo" />
+          <p>Hello</p>
+        {/* </Link> */}
+        </div>
       <div className="navbarItems">
         <div className="navbarItem">
             Hola mundo
         </div>
       </div>
-    </div>
   </div>
 
 
