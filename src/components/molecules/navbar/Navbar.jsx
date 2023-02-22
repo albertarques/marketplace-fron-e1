6e06fdce6d682b5dd5bd6b7c11b4fe9d0fec000d
Ana <117080170+anavgg@users.noreply.github.com>
2023-02-22 17:02:17 +0100

    nav

---
+++ src/components/molecules/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => (
+  <div className="navbarContainer">
+    <div className="navbar">
+      <div className="navbarBrand">
+      </div>
+      <div className="navbarItems">
+        <div className="navbarItem">
+            Hola mundo
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+);
+
+export default Navbar;
