0e4663a4587352fd9961f18afddbde4333650dee
Albert Arques <albertarques@gmail.com>
2023-02-23 06:46:12 +0100

    Relocated archives

---
+++ src/components/interface/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+// import { Link } from "react-router-dom";
+import Logo from '../../../assets/img/logo.png';
+import style from '../navbar/navbar.module.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import Search from '../search/Search';
+
+const Navbar = () => (
+  <div className={style.navbarContainer}>
+    <div className={style.navbarLogo}>
+      {/* <Link to="/"> */}
+      <img className={style.img} src={Logo} alt='Logo' />
+      <h1>Visual Side</h1>
+
+      {/* </Link> */}
+    </div>
+    <div className={style.navbarText}></div>
+    <div className={style.navbarLink}>
+      <ul className={style.navbarUl}>
+        <li>Crear anuncio</li>
+        <li>Mis favoritos</li>
+      </ul>
+    </div>
+    <Search />
+    
+    {/* <div>
+      <input className={style.navbarInput} type='text' name='search' />
+    </div>
+ */}
+    <div className={style.navBbarMenu}>
+      {/* <GiHamburgerMenu className={style.hamburger} /> */}
+    </div>
+  </div>
+);
+
+export default Navbar;
