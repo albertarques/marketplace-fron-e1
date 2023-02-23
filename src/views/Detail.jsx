b2f9e12de3cd86501e7f0375bb5d3e0e2627da0b
Albert Arques <albertarques@gmail.com>
2023-02-23 22:24:37 +0100

    Update package json

---
+++ src/views/Detail.jsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import Navbar from "../components/interface/navbar/Navbar";
+import Footer from "../components/interface/footer/Footer";
+
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Container } from "@mui/system";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Detail() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <Container>
+        <Card>
+          <CardMedia
+            component="img"
+            height="194"
+            image="/static/images/cards/paella.jpg"
+            alt="Image"
+          />
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              Name of the advertiser.
+            </Typography>
+          </CardContent>
+
+          <CardActions disableSpacing></CardActions>
+
+          <CardContent>
+            <Typography paragraph>Job Title</Typography>
+            <Typography paragraph>Web</Typography>
+            <Typography paragraph>Location</Typography>
+            <Typography paragraph>Contact</Typography>
+            <Typography paragraph>Job Description.</Typography>
+            <Typography paragraph>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source. Lorem Ipsum comes
+              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
+              BC. This book is a treatise on the theory of ethics, very popular
+              during the Renaissance. The first line of Lorem Ipsum, "Lorem
+              ipsum dolor sit amet..", comes from a line in section 1.10.32.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
+
+      <Footer />
+    </>
+  );
+}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 
 import Navbar from '../components/interface/navbar/Navbar'
 import Footer from '../components/interface/footer/Footer'
@@ -14,7 +14,6 @@ import Card from "@mui/material/Card";
 import Box from "@mui/material/Card";
 import { Container } from "@mui/system";
 
-import data from '../data/products.json'
 
 function Home() {
 
