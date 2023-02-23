2792fa46f6205f1818231776021b19e53dfe632e
Albert Arques <albertarques@gmail.com>
2023-02-23 15:06:28 +0100

    Deleted JobsProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 
 import PublicRoutes from "./router/PublicRoutes";
-import { JobsProvider } from "./context/JobsContext";
 
 function App() {
   return (
--- src/router/PublicRoutes.jsx
+++ src/router/PublicRoutes.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { JobsProvider } from "../context/JobsContext";
-
 import Home from "../views/Home";
 
 function PublicRoutes() {
   return (
-    <JobsProvider>
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
         </Routes>
       </Router>
-    </JobsProvider>
 );
 }
 
