4c1d459acdb3d7aebc325c36dd26100cc73e9888
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-02-23 11:54:50 +0100

    add hamburger menu

--- src/components/interface/navbar/Navbar.jsx
+++ src/components/interface/navbar/Navbar.jsx
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Link } from "react-router-dom";
 import Logo from '../../../assets/img/logo.png';
 import style from '../navbar/navbar.module.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import Search from '../search/Search';
 
-const Navbar = () => (
-  <div className={style.navbarContainer}>
-    <div className={style.navbarLogo}>
-      {/* <Link to="/"> */}
-      <img className={style.img} src={Logo} alt='Logo' />
-      <h1>Visual Side</h1>
+const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
-      {/* </Link> */}
+  const handlerMenu = () => {
+    setIsVisible(!isVisible);
+  };
+  return (
+    <div>
+      <div className={style.navbarContainer}>
+        <div className={style.navbarLogo}>
+          {/* <Link to="/"> */}
+          <img className={style.img} src={Logo} alt='Logo' />
+          <h1>Visual Side</h1>
+
+          {/* </Link> */}
+        </div>
+
+        <nav className={isVisible ? style.visible : style.invisible}>
+          <div className={style.navbarLink}>
+            <ul className={style.navbarUl}>
+              <li>Crear anuncio</li>
+              <li>Mis favoritos</li>
+              <li>Home</li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className={style.navBbarMenu}>
+          <GiHamburgerMenu onClick={handlerMenu} className={style.hamburger} />
+        </div>
+      </div>
+      <Search className={style.inputSearch} />
     </div>
-    <div className={style.navbarText}></div>
-    <div className={style.navbarLink}>
-      <ul className={style.navbarUl}>
-        <li>Crear anuncio</li>
-        <li>Mis favoritos</li>
-      </ul>
-    </div>
-    <Search />
-    
-    {/* <div>
-      <input className={style.navbarInput} type='text' name='search' />
-    </div>
- */}
-    <div className={style.navBbarMenu}>
-      {/* <GiHamburgerMenu className={style.hamburger} /> */}
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
