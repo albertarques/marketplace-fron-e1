c074e4b0b2d3fb95aea8769729b94ceebc8ef945
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-02-23 15:05:53 +0100

    pagina detail primera opcion

--- src/router/PublicRoutes.jsx
+++ src/router/PublicRoutes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { JobsProvider } from "../context/JobsContext";
 
 import Home from "../views/Home";
+import Detail from "../pages/Detail";
 
 function PublicRoutes() {
   return (
@@ -11,6 +12,7 @@ function PublicRoutes() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/detail" element={<Detail />}></Route>
         </Routes>
       </Router>
     </JobsProvider>
