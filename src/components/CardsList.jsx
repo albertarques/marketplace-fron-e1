a08366c1cf0ffc8f454b0d24209b0a3320a0b88e
Albert Arques <albertarques@gmail.com>
2023-02-23 17:08:37 +0100

    Create DataContext and list items in Home page

---
+++ src/components/CardsList.jsx
@@ -0,0 +1,51 @@
+import React, {useContext} from 'react'
+
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import { Container } from "@mui/system";
+
+import {DataContext} from '../context/DataContext'
+
+function CardsList() {
+
+  const { data } = useContext(DataContext);
+
+  if(data){
+    return (
+        <Container fixed>
+          <Grid container spacing={2}>
+            {data.jobs.map((product) => (
+              <Card grid key={product.key} sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  sx={{ height: 140 }}
+                  image={product.thumbnail}
+                  title={product.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {product.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {product.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Share</Button>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            ))}
+          </Grid>
+        </Container>
+    )
+  } else {
+    return <h2>No data is available</h2>
+  }
+}
+
+export default CardsList
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,59 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 
 import Navbar from '../components/interface/navbar/Navbar'
 import Footer from '../components/interface/footer/Footer'
+import CardsList from '../components/CardsList'
 
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-
-import Card from "@mui/material/Card";
-import { Container } from "@mui/system";
-import jobsService from '../apiService/jobsService'
 
 function Home() {
-
-  const [data, setData] = useState([]);
-
-
-  useEffect(() => {
-    jobsService.getAll().then((res) => {
-      setData(res);
-    });
-  }, []);
 
   return (
     <>
       <Navbar />
-      <Container fixed>
-        <Grid container spacing={2}>
-          {console.log(data.jobs)}
-          {/* {data.jobs.map((product) => (
-            <Card key={product.key} sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image={product.thumbnail}
-                title={product.title}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {product.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {product.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          ))} */}
-        </Grid>
-      </Container>
+      <CardsList />
       <Footer />
     </>
   )
